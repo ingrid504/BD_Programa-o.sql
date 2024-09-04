5336218da1dc68a5e09bffe11622dcbf74932805
Ingrid <ingmoroliveira3374@gmail.com>
2024-09-04 15:48:41 -0300

    Create 04_Programação.sql

---
+++ 04_Programação.sql
@@ -0,0 +1,14 @@
+INSERT INTO Clientes (nome, sobrenome, email, telefone, endereco)
+VALUES
+INSERT INTO Clientes (nome, sobrenome, email, telefone, endereco)
+VALUES
+('João','Silva','joao.silva@example.com','123456789','Rua 1, 123'),
+('Maria','Souza','maria.souza@example.om','987654321','Rua 2, 456'),
+('Pedro','Oliveira','pedro.oliveira@example.com','111222333','Rua 3,789'),
+('Ana','Santos','ana.santos@example.com','444555666','Rua 4,012'),
+('Luiz','Gomes','luiz.gomes@example.com','777888999','Rua 5 345'),
+('Beatriz','Rocha','beatriz.rocha@example.com','222333444','Rua 6,678'),
+('Carlos','Mendes','carlos.mendes@example.com','555666777','Rua 7,901'),
+('Eduardo','Ferreira','eduardo.ferreira@example.com','888999000','Rua 8,234'),
+('Gabriela','Lima','gabriela.lima@example.com','333444555','Rua 9,567'),
+('Hugo','Araújo','hugo.araujo@example.com','666777888','Rua 10,890');
